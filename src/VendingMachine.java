66b56cbf1d94ad32300fa73449c967faa2260bfb
Cameron Jett <jett.18@osu.edu>
2017-06-09 04:00:23 -0400

    Initial Commit Created test and class

---
+++ src/VendingMachine.java
@@ -0,0 +1,12 @@
+
+public class VendingMachine {
+    private String display;
+
+    public VendingMachine() {
+        this.display = "INSERT COIN";
+    }
+
+    public String getDisplay() {
+        return display;
+    }
+}
