fba0d5a227e5f7f19563c8771ee6fb4d5f5ca584
Cameron Jett <jett.18@osu.edu>
2017-06-10 01:22:38 -0400

    Refactored VendingMachineTest with setup method

--- test/VendingMachineTest.java
+++ test/VendingMachineTest.java
@@ -1,18 +1,23 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
 
 public class VendingMachineTest {
+    VendingMachine vendingMachine;
+
+    @Before
+    public void setUp() {
+        vendingMachine = new VendingMachine();
+    }
 
     @Test
     public void whenVendingMachineHasNoCoinsItDisplaysInsertCoin() {
-        VendingMachine vendingMachine = new VendingMachine();
         assertEquals("INSERT COIN", vendingMachine.getDisplay());
     }
 
     @Test
     public void whenVendingMachineReceivesNickelItDisplaysFiveCents() {
-        VendingMachine vendingMachine = new VendingMachine();
         vendingMachine.insert("NICKEL");
         assertEquals("$ 0.05", vendingMachine.getDisplay());
     }
