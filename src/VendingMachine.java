05a00a562cc4c1f0d608b3e4c1fe55a82ad9d40e
Cameron Jett <jett.18@osu.edu>
2017-06-10 01:18:22 -0400

    Finished vending machine accepting nickels

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -1,12 +1,23 @@
 
 public class VendingMachine {
     private String display;
+    private double currentAmount;
 
     public VendingMachine() {
         this.display = "INSERT COIN";
     }
 
     public String getDisplay() {
+        display = "INSERT COIN";
+        if (currentAmount > 0) {
+            display = "$ " + currentAmount;
+        }
         return display;
+    }
+
+    public void insert(String coin) {
+        if (coin.equals("NICKEL")) {
+            currentAmount += .05;
+        }
     }
 }
--- test/VendingMachineTest.java
+++ test/VendingMachineTest.java
@@ -3,9 +3,17 @@ import org.junit.Test;
 import static junit.framework.TestCase.assertEquals;
 
 public class VendingMachineTest {
+
     @Test
     public void whenVendingMachineHasNoCoinsItDisplaysInsertCoin() {
         VendingMachine vendingMachine = new VendingMachine();
         assertEquals("INSERT COIN", vendingMachine.getDisplay());
+    }
+
+    @Test
+    public void whenVendingMachineReceivesNickelItDisplaysFiveCents() {
+        VendingMachine vendingMachine = new VendingMachine();
+        vendingMachine.insert("NICKEL");
+        assertEquals("$ 0.05", vendingMachine.getDisplay());
     }
 }
